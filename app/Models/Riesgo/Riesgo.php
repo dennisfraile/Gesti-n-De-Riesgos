2500eb72715bb8b559403191a26ff093c4178bdc
WalterVelis <waltervelis45@gmail.com>
2017-10-11 21:47:12 -0600

    Agregada plantilla de Boostrap
    modificacion de estilo-

--- app/Models/Riesgo/Riesgo.php
+++ app/Models/Riesgo/Riesgo.php
@@ -6,12 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Riesgo extends Model
 {
-    //
+
     protected $table = 'riesgo';
+
     protected $primparykey = 'idriesgo';
-    protected $fillable = ['idriesgo', 'descripriesgo', 'estimacion','impacto'
-    , 'idactivo','idvulnerabilidad'];
+
     public $timestamps =false;
+
+    protected $fillable = [
+            'idriesgo', 
+            'descripriesgo', 
+            'estimacion',
+            'impacto',
+            'idactivo',
+            'idvulnerabilidad'
+            ];
+
     public function activos(){
         return $this->belongsto(Activo::class);
     }
