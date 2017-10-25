9c8925e7f4422d7e6d8588790ebd30dc5c042729
brallansanchez <brsr1234@gmail.com>
2017-10-25 16:16:23 -0600

    4f2111eb7cbff58d550c26e6b7e4969ffd8bf2f7

---
+++ app/Http/Controllers/TratamientoRiesgo/TipoTratamientoRiesgoControlles.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace GestionDeRiesgos\Http\Controllers\TratamientoRiesgo;
+
+use Illuminate\Http\Request;
+
+use GestionDeRiesgos\Http\Requests;
+use GestionDeRiesgos\Http\Controllers\Controller;
+use GestionDeRiesgos\Models\TipoTratamiento;
+use Session;
+use DB;
+
+class TipoTratamientoRiesgoControlles extends Controller
+{
+    public function __construct()
+    {
+
+    }
+    public function index(Request $request)
+    {
+        if($request)
+        {
+            $tipotratamientos=DB::table('tipotratamiento as tt')
+            ->select('tt.idtipotratamiento','tt.nombretipotrata','tt.descriptipotrata')
+            ->get();
+            return 
+            view('tt.tipoactivo', ["tipotrattamientos"=>$tipotratamientos]);
+            
+        }
+                
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return 
+        view("tt.create");
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tipotratamiento=new TipoTratamiento;
+        $tipotratamiento->idtipotratamiento=$request->get('idtipotratamiento');
+        $tipotratamiento->nombretipotrata=$request->get('nombretipotrata');
+        $tipotratamiento->descriptipotrata=$request->get('descriptipotrata');
+
+        $tipotratamiento->save();
+        
+        return Redirect::to('tipotratamiento');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        return  view("tt.edit", ["tipotratamiento"=>TipoTratamiento::findOrFail($id)]);
+
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $affectedRows = TipoTratamiento::where('idtipotratamiento','=',$id)
+        ->update([
+            'nombretipotrata'=>$request->get('nombretipotrata'),
+            'descriptipotrata'=>$request->get('descriptipotrata')]
+        );
+        return Redirect::to('tipotratamiento');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $affectedRows = TipoTratamiento::where('idtipotratamiento','=',$id)->delete();
+        return Redirect::to('tipotratamiento');
+    }
+}
---
+++ resources/views/tipotratamiento/create.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.admin')
+@section('contenido')
+<div class="row">
+   <div class="col-lg-12">
+   <ol class="breadcrumb">
+      <li> <i class="fa fa-home"></i>
+       <a href="{{url('/tipotratramiento')}}">
+      Administrar Tipos de Tratamientos</a>
+      </li>
+      <li class="active">
+      <i class="fa fa-desktop"></i> Nuevo Tipo de Tratamiento</li>
+    </ol>
+   </div>
+</div>
+
+<div class="row">
+   <div class="col-lg-12">
+         <h3>Nuevo Tipo de Tratamiento</h3>
+   </div>
+</div>
+
+
+{!!Form::open(array('url'=>'tipotratamiento','method'=>'POST','autocomplete'=>'off'))!!}
+   {{Form::token()}}
+
+<div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+      
+
+         <div class="form-group">
+               <label for="nombretipotrata">Nombre del Tipo de Tratamiento</label>
+               <input type="text" name="nombretipotrata" required value="{{old('nombretipotrata')}}" class="form-control" placeholder="Nombre...">
+         </div>
+
+           <div class="form-group">
+               <label for="descriptipotrata">Descripcion</label>
+               <input type="text" name="descriptipotrata" required value="{{old('descriptipotrata')}}" class="form-control" placeholder="Descripcion...">
+         </div>
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+
+         <div class="form-group">
+               <button class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-floppy-disk"></i> Guardar</button>
+               <a href="{{url('tipotratramiento')}}" class="btn btn-danger" role="button"><i class="glyphicon glyphicon-remove-circle"></i> Cancelar</a>
+         </div>
+
+       </div>
+
+   </div> 
+
+{!!Form::close()!!}           
+               
+@endsection
